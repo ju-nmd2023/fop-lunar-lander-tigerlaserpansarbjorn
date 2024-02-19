9d2c6aa53bf2893f156b8be42262e63fbd004dff
melvinkjellen <kjme23ny@student.ju.se>
2024-02-19 20:14:18 +0100

    Nightsky

--- lunar-lander.js
+++ lunar-lander.js
@@ -8,4 +8,35 @@ function spaceShip() {
     ellipse(spacex + 165, spacey + 65, 130);
 }
 
-spaceShip();
+//Stole from Garrit's site
+function spaceBackground(){
+    push();
+    noStroke();
+    background(0);
+    for (let index in starX) {
+        fill(255, 255, 255, Math.abs(Math.sin(starAlpha[index])) * 255);
+        ellipse(starX[index], starY[index], 2);
+        starAlpha[index] = starAlpha[index] + 0.008;
+    }
+    pop();
+}
+
+
+
+let starX = [];
+let starY = [];
+let starAlpha = [];
+
+for (let i = 0; i < 1000; i++) {
+    const x = Math.floor(Math.random() * width);
+    const y = Math.floor(Math.random() * height);
+    const alpha = Math.random();
+
+    starX.push(x);
+    starY.push(y);
+    starAlpha.push(alpha);
+}
+
+function draw() {
+    spaceBackground();
+}
